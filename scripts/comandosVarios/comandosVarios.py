61b2d39ac5c7dc544be34b318f5cea0b5edc5a15
MarcosNoDeFotos <ma.pega01@gmail.com>
2025-11-01 17:18:23 +0100

    se actualiza !comandos

--- scripts/comandosVarios/comandosVarios.py
+++ scripts/comandosVarios/comandosVarios.py
@@ -4,7 +4,7 @@ COMANDOS = {
     "!pc": "Pc Gaming: https://es.pc-builder.io/builds/D5CNOrHv | PC Streaming: https://es.pc-builder.io/builds/i6iKN0kj",
     "!donar": "¿Quieres ayudarme? Te lo agradezco mucho!! Visita este enlace https://streamlabs.com/marcosnodefotos o hazlo por PayPal (https://paypal.me/marcosspg)",
     "!discord": "Entra en mi discord y entérate de novedades y demás. Te espero allí!! https://discord.gg/e427a5r",
-    "!comandos": "!redes, !pc, !donar, !discord, !calva, !gamble",
+    "!comandos": "!redes, !pc, !donar, !discord, !calva, !gamble. ¿Cómo funcionan los comandos y Monedas Calvas?: https://github.com/MarcosNoDeFotos/youtube-chatbot/blob/main/Readme.md",
 }
 
 def command_listener(message, author, db, bot):
